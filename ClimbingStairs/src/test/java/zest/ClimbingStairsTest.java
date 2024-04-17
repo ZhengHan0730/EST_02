665291d5caf285bb152c871876734f23dc030b54
Zisen Liu <karl.liu.1024@gmail.com>
2024-04-17 16:29:23 +0200

    Revert "lzs"

--- ClimbingStairs/src/test/java/zest/ClimbingStairsTest.java
+++ ClimbingStairs/src/test/java/zest/ClimbingStairsTest.java
@@ -1,27 +1,8 @@
 package zest;
 
-import org.junit.jupiter.api.Test;
-
-import net.jqwik.api.ForAll;
-import net.jqwik.api.Property;
-import net.jqwik.api.constraints.IntRange;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ClimbingStairsTest {
 
-    private ClimbingStairs instance = new ClimbingStairs();
-
-    @Test
-    void foo() {
-        assertEquals(instance.climbStairs(1), 1);
-        assertEquals(instance.climbStairs(2), 2);
-    }
-
-    @Property(seed = "114514")
-    void equalsToOneBeforePlusTwoBefore(@ForAll @IntRange(min = 3) int numStairs) {
-        assertEquals(
-                instance.climbStairs(numStairs),
-                instance.climbStairs(numStairs - 1) + instance.climbStairs(numStairs - 2));
-    }
 }
+
+
